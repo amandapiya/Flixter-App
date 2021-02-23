4b063ca2aed507ac4b6876480adfdd581a26162c
amandapiya <apiyapan@caltech.edu>
2021-02-23 16:57:37 -0600

    Initial Commit

---
+++ App_Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_Flix
+//
+//  Created by Amanda P on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
