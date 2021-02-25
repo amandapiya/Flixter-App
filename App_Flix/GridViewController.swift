428616f32a9a7c081fdc34dc3f12b5be3076cc11
amandapiya <apiyapan@caltech.edu>
2021-02-25 13:06:40 -0600

    week 1

---
+++ App_Flix/GridViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GridViewController.swift
+//  App_Flix
+//
+//  Created by Amanda P on 2/25/21.
+//
+
+import UIKit
+
+class GridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
